a82afa98364c34d15ac511918add66e360c08300
EBIN BABU <ebin.babu.in@icloud.com>
2026-05-03 13:22:55 +0530

    Add read-only API viewset for Prompt model: Add read-only API viewset for Image model

--- core/views.py
+++ core/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+
 from .models import Prompt,Image
 
 from .serializers import PromptSerializer, ImageSerializer
 
 
-class PromptViewSet(viewsets.ModelViewSet):
+class PromptViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Prompt.objects.all()
     serializer_class = PromptSerializer
+    permission_classes = [AllowAny]
 
-class ImageViewSet(viewsets.ModelViewSet):
+class ImageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    permission_classes = [AllowAny]
 
