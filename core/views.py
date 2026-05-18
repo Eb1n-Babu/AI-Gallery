3cc59fad930647d41f87d15385f07e13a907e03e
EBIN BABU <ebin.babu.in@icloud.com>
2026-05-18 17:07:30 +0530

    project updated

--- core/views.py
+++ core/views.py
@@ -1,18 +1,15 @@
+# views.py
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
-
-from .models import Prompt,Image
-
+from .models import Prompt, Image
 from .serializers import PromptSerializer, ImageSerializer
 
-
-class PromptViewSet(viewsets.ReadOnlyModelViewSet):
+class PromptViewSet(viewsets.ModelViewSet):   # changed from ReadOnlyModelViewSet
     queryset = Prompt.objects.all()
     serializer_class = PromptSerializer
     permission_classes = [AllowAny]
 
-class ImageViewSet(viewsets.ReadOnlyModelViewSet):
+class ImageViewSet(viewsets.ModelViewSet):    # changed from ReadOnlyModelViewSet
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     permission_classes = [AllowAny]
-
